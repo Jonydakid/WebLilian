746df002e6ade811ff8a32d830a35450b4cb92e3
Beiconn69 <46577792+Beiconn69@users.noreply.github.com>
2019-01-16 16:49:31 -0300

    Update models.py

--- colaboraMas/webColaboraMas/models.py
+++ colaboraMas/webColaboraMas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    NomCurso = models.CharField(max_length=30)
+    Valor = models.IntegerField(max_length=20)
+
