fbcb03d6640f2f9e6588250bb4d6db903bc90a17
Jonathan Torres <jonathan.torres.salvo@gmail.com>
2019-01-17 14:00:45 -0300

    añadido forms

--- colaboraMas/webColaboraMas/forms.py
+++ colaboraMas/webColaboraMas/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+from .models import Curso
+class FormContacto(forms.Form):
+    
+    nombre = forms.CharField(label="Nombre completo", max_length=120, widget=forms.TextInput(
+        attrs={"id": "nombres", "class": "campo", "placeholder": "Ingrese nombres"}))
+    curso = forms.ModelChoiceField(label = "Curso", queryset = Curso.objects.all(), empty_label = None, widget = forms.Select(attrs = { "id": "curso"}))
+    correo = forms.EmailField(label="Correo electrónico", max_length=30, widget=forms.EmailInput(
+    	attrs={"id": "correo", "class": "campo", "placeholder": "direccioncorreo@correo.com"}))
+    mensaje = forms.CharField(label="Mensaje", required=True, widget=forms.TextInput(
+    	attrs={"id": "mensaje", "class": "campo", "placeholder": "Ingrese Mensaje"}))
