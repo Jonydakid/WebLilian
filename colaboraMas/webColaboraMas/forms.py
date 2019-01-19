ab2e5e52fd2c75f2e3365feaec43bb976ce7bd1a
Jonathan Torres <jonathan.torres.salvo@gmail.com>
2019-01-19 12:44:15 -0300

    contacto hecho

--- colaboraMas/webColaboraMas/forms.py
+++ colaboraMas/webColaboraMas/forms.py
@@ -3,7 +3,7 @@ from .models import Curso
 class FormContacto(forms.Form):
     
     nombre = forms.CharField(label="Nombre completo", max_length=120, widget=forms.TextInput(
-        attrs={"id": "nombre", "class": "campo", "placeholder": "Ingrese nombres"}))
+        attrs={"id": "nombre", "class": "campo", "placeholder": "Ingrese nombre"}))
     correo = forms.EmailField(label="Correo electrónico", max_length=30, widget=forms.EmailInput(
     	attrs={"id": "correo", "class": "campo", "placeholder": "direccioncorreo@correo.com"}))
     curso = forms.ModelChoiceField(label = "Curso", queryset = Curso.objects.all(), empty_label = None, widget = forms.Select(attrs = { "id": "curso"}))
